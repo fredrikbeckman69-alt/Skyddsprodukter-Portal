4a7e98e44926590420870f353768b5cafa52212b
Fredrik Beckman <fredrik.beckman69@gmail.com>
2026-03-11 15:55:46 +0100

    fix: github pages actions workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Necessary for GitHub Pages compatibility
+  base: '/Skyddsprodukter-Portal/', // Necessary for GitHub Pages compatibility
 })
